2e9535b48a297464f849631729d0a039f6d2f968
arikowira <arikowirasetya@gmail.com>
2023-03-07 10:47:23 +0700

    update module with parameter age

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/arikowira/go-say-hello
+module github.com/arikowira/go-say-hello/v2
 
 go 1.20
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package say_hello
 
-func SayHello(name string) string {
-	return "Hello My Friend " + name
+func SayHello(name string, age string) string {
+	return "Hello My Friend " + name + "and My Age " + age + " Years Old"
 }
